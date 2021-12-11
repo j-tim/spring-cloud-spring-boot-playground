7ff8f1bd88640a778b609cc6e228fbf412941d71
Tim <tim@jtim.nl>
2021-12-11 16:34:05 +0100

    Example with Spring Cloud 2021.0.0 and Spring Boot 2.6.1

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
-./mvnw -pl config-server spring-boot:build-image
-./mvnw -pl service-a spring-boot:build-image
-./mvnw -pl service-b spring-boot:build-image
-./mvnw -pl service-registry spring-boot:build-image
-./mvnw -pl spring-boot-admin spring-boot:build-image
+./mvnw -pl config-server spring-boot:build-image -DskipTests=true
+./mvnw -pl service-a spring-boot:build-image -DskipTests=true
+./mvnw -pl service-b spring-boot:build-image -DskipTests=true
+./mvnw -pl service-registry spring-boot:build-image -DskipTests=true
+./mvnw -pl spring-boot-admin spring-boot:build-image -DskipTests=true
